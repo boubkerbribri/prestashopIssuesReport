e28183d00a0189c07c971b5b1218161b76902c57
Simon Garny <simon.garny@prestashop.com>
2020-05-12 16:48:25 +0200

    add markdown file generation

--- generate.php
+++ generate.php
@@ -1,6 +1,8 @@
 <?php
-
 include("run.php");
+
+$report_directory = __DIR__ . '/reports/';
+
 if (getenv('START_DATE') === false) {
     throw new Exception("START_DATE var is mandatory.");
 }
@@ -21,6 +23,10 @@ if (getenv('END_DATE') === false) {
 $start_date_formatted = date('Y-m-d', $start_date);
 $end_date_formatted = date('Y-m-d', $end_date);
 
+
+$report_filename = 'Report-'.$start_date_formatted.'-'.$end_date_formatted.'.md';
+$report_path = $report_directory.$report_filename;
+
 //get all issues between these dates
 $parameters = [
     'type:issue created:'.$start_date_formatted.'..'.$end_date_formatted.' sort:created-desc repo:prestashop/prestashop'
@@ -29,6 +35,7 @@ $parameters = [
 
 $filename = 'result.json';
 $f = fopen($filename, 'r');
+
 $issues = json_decode(fread($f, filesize($filename)), TRUE);
 
 echo "Found " . count($issues) . " issues" . PHP_EOL;
@@ -75,7 +82,6 @@ echo "Retrieving duplicates data...".PHP_EOL;
 $duplicates = $results['duplicates'];
 $pattern = '/Duplicates? of #(\d+)/i';
 $original_issues = [];
-$i = 1;
 foreach($duplicates as $duplicate) {
     //get all comments
     $comments = $client->api('issue')->comments()->all('PrestaShop', 'PrestaShop', $duplicate['number']);
@@ -89,15 +95,68 @@ foreach($duplicates as $duplicate) {
             if (!isset($original_issues[$original_issue_number])) {
                 $original_issues[$original_issue_number] = $original_issue;
             }
-            $original_issues[$original_issue_number]['regressions'][] = $duplicate;
+            $original_issues[$original_issue_number]['duplicates'][] = $duplicate;
             break;
         }
     }
-    $i++;
 }
+$results['duplicates'] = $original_issues;
 
 echo "Open : ".$results['open'].PHP_EOL;
 echo "Closed : ".$results['closed'].PHP_EOL;
 echo "Duplicates : ".count($results['duplicates']).PHP_EOL;
 echo "Regressions : ".count($results['regressions']).PHP_EOL;
 echo "    Dont regressions TE : ".$results['regressions_TE'].PHP_EOL;
+
+$template_content = file_get_contents('template.md');
+$replacement_data = [
+    'start-date' => $start_date_formatted,
+    'end-date' => $end_date_formatted,
+    'period' => ceil(($end_date - $start_date)/(24*3600)).' days',
+    'issues-created' => count($issues),
+    'issues-open' => $results['open'],
+    'issues-closed' => $results['closed'],
+    'issues-duplicates' => count($results['duplicates']),
+    'issues-duplicates-percentage' => round((count($results['duplicates']) / count($issues) * 100), 2),
+    'issues-regressions' => count($results['regressions']),
+    'issues-regressions-percentage' => round((count($results['regressions']) / count($issues) * 100), 2),
+    'issues-detected-by-te' => $results['regressions_TE'],
+    'issues-detected-by-te-percentage' => round(($results['regressions_TE'] / count($issues) * 100), 2),
+];
+$duplicate_table = '';
+foreach($results['duplicates'] as $source) {
+    $priority = '-';
+    $bo_fo = '-';
+    foreach($source['labels'] as $label) {
+        if (in_array($label['name'], ['Trivial', 'Minor', 'Major', 'Critical'])) {
+            $priority = $label['name'];
+        }
+        if ($label['name'] == 'FO') {
+            $bo_fo = 'FO';
+        }
+        if ($label['name'] == 'BO') {
+            $bo_fo = 'BO';
+        }
+    }
+    foreach($source['duplicates'] as $duplicate) {
+        $duplicate_data = [
+            '['.$duplicate['number'].']('.$duplicate['html_url'].')',
+            $duplicate['title'],
+            $bo_fo,
+            '['.$source['number'].']('.$source['html_url'].')',
+            $source['state'],
+            $priority,
+        ];
+        $duplicate_table .= '|'.implode('|', $duplicate_data).'|'.PHP_EOL;
+    }
+}
+
+$replacement_data['duplicate-table'] = $duplicate_table;
+
+foreach($replacement_data as $k => $v) {
+    $template_content = str_replace("%$k%", $v, $template_content);
+}
+
+$handle = fopen($report_path, 'w');
+fwrite($handle, $template_content);
+echo "Report $report_filename written.".PHP_EOL;
---
+++ template.md
@@ -0,0 +1,14 @@
+# Issue report (%start-date% - %end-date%)
+
+## Recurrent bugs
+
+| # | Title | BO/FO | Source | Source status | Source priority |
+|---|-------|-------|--------|---------------|-----------------|
+%duplicate-table%
+
+## Informations
+- Period : %period%
+- Issues created : %issues-created% (%issues-open% open / %issues-closed% closed)
+- Duplicates : %issues-duplicates% (%issues-duplicates-percentage%% of all issues)
+- Regressions : %issues-regressions% (%issues-regressions-percentage%% of all issues)
+- Detected by automated tests : %issues-detected-by-te% (%issues-detected-by-te-percentage%% of all issues)
